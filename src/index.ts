1bb062bb2362d649ef94eb8f33dd081a5970bb8e
a3510377 <a102009102009@gmail.com>
2022-03-01 23:30:48 +0800

    Emm

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
-const main = (data: any, ...args: string[]): string =>
-  args.join("").replace(/[^\x00-\xFF]/g, (s) => (s in data ? data[s] : s));
+import { cn_tw as cn_twData } from "./data/cn_tw";
+import { tw_cn as tw_cnData } from "./data/tw_cn";
 
-export const toSimp = (...args: string[]): string => main("", ...args);
+export type dataType = typeof cn_twData | typeof tw_cnData;
 
-export const toTrad = (...args: string[]): string => main("", ...args);
+const main = (data: dataType, ...args: string[]): string =>
+  args
+    .join("")
+    .replace(/[^\x00-\xFF]/g, str =>
+      str in data ? data[<keyof dataType>str] : str
+    );
+
+/**將 args 轉為 簡體中文
+ * @param {string[]} args 繁體中文
+ * @return {string} 簡體中文
+ */
+export const toCN = (...args: string[]): string => main(tw_cnData, ...args);
+
+/**將 args 轉為 繁體中文
+ * @param {string[]} args 簡體中文
+ * @return {string} 繁體中文
+ */
+export const toTW = (...args: string[]): string => main(cn_twData, ...args);
